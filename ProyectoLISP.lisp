2e60ce33ee5f84a906ffcc4e872a598f5f27c908
SergioAndrade22 <sergio.david2201@gmail.com>
2019-06-10 14:06:03 -0300

    Fix bug Arregle el sumaPrimos que antes daba siempre 10.

--- ProyectoLISP.lisp
+++ ProyectoLISP.lisp
@@ -1,39 +1,44 @@
+; Función que calcula la suma de todos los números naturales primos hasta un N dado. Hasta un N = 3352
 (DEFUN sumaPrimos (N)
 	(COND
-		((= N 0) 
+		((< N 0) ; el programa no considera la suma de numeros primos en "sentido negativo"
+			NIL
+		)
+		((= N 0)  ; si N es 0, corta la recursión se devuelve 0
 			0
 		)
-		((esPrimoShell N) 
+		((isPrimeShell N) ; si el N actual es primo, la suma de primos de N es igual a N más la suma de primos de N-1
 			(+ N (sumaPrimos (- N 1)))
 		)
-		(T 
+		(T ; si N no es primo, la suma de primos de N es igual a la suma de primos de N-1
 			(sumaPrimos (- N 1))
 		)
 	)
 )
 
-(DEFUN esPrimoShell (N)
-	(LET ((A 2) (B (/ N 2)))
-		(esPrimo N A B)
+; Función cáscara auxiliar a la función isPrime
+(DEFUN isPrimeShell (N)
+	(LET ((B 2))
+		(isPrime N B)
 	)
 )
-
-(DEFUN esPrimo (N A B)
+; Función auxiliar que calcula si un número dado "N" es primo, "B" es pasado por parámetro y empieza en 2. Se considera que 1 NO es primo.
+(DEFUN isPrime (N B)
 	(COND
-		((> B A)
+		((= N 1) ; caso base de los "N", 1 no se considera número primo
+			NIL
+		)
+		((< B (/ (+ N 1) 2))  ; estoy en un caso en el cual tengo que evaluar si N es primo. No existe B tal que B > N/2 y B es divisor de B
 			(COND
-				((= (REM N B) 0)
+				(( = (MOD N B) 0) ; si ocurre que el módulo (MOD) de N y B es 0, entonces N es divisible por B por lo cuál no es primo
 					NIL
 				)
-				((= (REM N A) 0)
-					NIL
-				)
-				(T
-					(esPrimo N (+ A 1) (- B 1))
+				(T ; si N no era divisible por B debo verificar que no sea divisible por B+1
+					(isPrime N (+ B 1))
 				)
 			)
 		)
-		(T
+		(T ; llega el caso en que B >= N por lo que N es primo
 			T
 		)
 	)

0f5cd58a4a7393fb71c723b31929fa0cb370bf70
SergioAndrade22 <sergio.david2201@gmail.com>
2019-06-11 05:59:24 -0300

    Añadida funcion trans
    Añadi la funcion trans con sus auxiliares, todas ya funcionales, comentadas y robustas. Añadi robustez a la funcion sumaPrimos

--- ProyectoLISP.lisp
+++ ProyectoLISP.lisp
@@ -1,17 +1,81 @@
+;-------------------------------------------EJERCICIO MATRIZ TRASPUESTA------------------------------------------
+; F-uncion que computa la traspuesta de una matriz representado como una lista de listas
+(DEFUN trans (M)
+	(COND
+		((LISTP M) ; primero se verifica que M sea efectivamente una lista
+			(COND
+				((NOT (LISTP (CAR M))) ; luego se verifica que M sea una representacion valida de una matriz
+					"La lista ingresada no es una matriz."
+				)
+				((NULL (CDR M)) ; si M es una matriz de una unica fila, la traspuesta de M es M
+					M
+				)
+				(T ; caso general
+					(COND
+						((NULL M) ; si M es NIL llego al caso base de la recursion
+							NIL
+						)
+						((NULL (CAR M)) ; verificacion que evita una recursion infinita, si en M se encuentra un NIL se asume que ese es el final de la matriz
+							NIL
+						)
+						(T ; caso recursivo que concatena la fila resultado de la instancia actual con el resultado de la siguiente instancia sobre M reducida en una fila
+							(CONS (compRow M) (trans (reduceMatrix M)))
+						)
+					)
+				)
+			)
+		)
+		(T ; en caso de que M no fuera una lista esta funcion no tiene sentido alguno
+			"Esta funcion fue implementada para operar sobre matrices representadas como lista de listas."
+		)
+	)
+)
+
+; Funcion auxiliar que permite obtener, a partir de una matriz M representada como lista de listas, una fila resultado de trasponer la matriz
+(DEFUN compRow (M)
+	(COND
+		((NULL M) ; si M es vacia, una fila traspuesta de una matriz vacia es una fila vacia
+			NIL
+		)
+		(T ; en caso contrario se concatena el primer elemento de la primer fila de la instancia actual de M, con el primer elemento de la primer fila de la instancia siguiente con M reducida en una fila
+			(CONS (CAR (CAR M)) (compRow (CDR M)))
+		)
+	)
+)
+
+; Funcion auxiliar que, dada una matriz M representada como lista de listas, elimina el primer elemento de cada fila
+(DEFUN reduceMatrix (M)
+	(COND
+		((NULL M) ; si M es vacia no necesito continuar reduciendo, devuelvo vacio
+			NIL
+		) 
+		(T ; caso contrario la reduccion de M es el resultado de concatenar la primer fila de M sin su primer elemento, con la reduccion sobre el cuerpo de M
+			(CONS (CDR (CAR M)) (reduceMatrix (CDR M)))
+		)
+	)
+)
+;-------------------------------------------EJERCICIO SUMAPRIMOS-------------------------------------------
 ; Función que calcula la suma de todos los números naturales primos hasta un N dado. Hasta un N = 3352
 (DEFUN sumaPrimos (N)
 	(COND
-		((< N 0) ; el programa no considera la suma de numeros primos en "sentido negativo"
-			NIL
+		((INTEGERP N)
+			(COND
+				((< N 0) ; el programa no considera la suma de numeros primos en "sentido negativo"
+					"Esta funcion espera recibir como argumento un entero mayor o igual a 0."
+				)
+				((= N 0)  ; si N es 0, corta la recursión se devuelve 0
+					0
+				)
+				((isPrimeShell N) ; si el N actual es primo, la suma de primos de N es igual a N más la suma de primos de N-1
+					(+ N (sumaPrimos (- N 1)))
+				)
+				(T ; si N no es primo, la suma de primos de N es igual a la suma de primos de N-1
+					(sumaPrimos (- N 1))
+				)
+			)
 		)
-		((= N 0)  ; si N es 0, corta la recursión se devuelve 0
-			0
-		)
-		((isPrimeShell N) ; si el N actual es primo, la suma de primos de N es igual a N más la suma de primos de N-1
-			(+ N (sumaPrimos (- N 1)))
-		)
-		(T ; si N no es primo, la suma de primos de N es igual a la suma de primos de N-1
-			(sumaPrimos (- N 1))
+		(T
+			"Esta funcion fue implementada para operar sobre enteros."
 		)
 	)
 )

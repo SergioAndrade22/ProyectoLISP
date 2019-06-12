bbb4d347f8177ad44404c7c0018769de009d0697
SergioAndrade22 <sergio.david2201@gmail.com>
2019-06-12 19:58:19 -0300

    Ahora terminamos ÃLe meti mas documentacion y anduve con el insecticida matando bugs

--- ProyectoLISP.lisp
+++ ProyectoLISP.lisp
@@ -1,5 +1,5 @@
 ;-------------------------------------------EJERCICIO MATRIZ TRASPUESTA------------------------------------------
-; Funcion que computa la traspuesta de una matriz representado como una lista de listas
+; Funcion que computa la traspuesta de una matriz representada como una lista de listas
 (DEFUN trans (M)
 	(COND
 		((LISTP M) ; primero se verifica que M sea efectivamente una lista
@@ -12,7 +12,7 @@
 				)
 				(T ; caso general
 					(COND
-						((NULL M) ; si M es NIL llego al caso base de la recursion
+						((= (LIST-LENGTH M) 0) ; si M se redujo a 0 finaliza la trasposicion
 							NIL
 						)
 						((NULL (CAR M)) ; verificacion que evita una recursion infinita, si en M se encuentra un NIL se asume que ese es el final de la matriz
@@ -31,7 +31,8 @@
 	)
 )
 
-; Funcion auxiliar que permite obtener, a partir de una matriz M representada como lista de listas, una fila resultado de trasponer la matriz
+; Funcion auxiliar que permite obtener, a partir de una matriz M representada como lista de listas, 
+; una fila resultado de concatenar el primer elemento de cada fila de la matriz
 (DEFUN compRow (M)
 	(COND
 		((NULL M) ; si M es vacia, una fila traspuesta de una matriz vacia es una fila vacia
@@ -58,9 +59,9 @@
 ; Función que calcula la suma de todos los números naturales primos hasta un N dado. Hasta un N = 3352
 (DEFUN sumaPrimos (N)
 	(COND
-		((INTEGERP N)
+		((INTEGERP N) ; ejecucion valida solo si N es un integer
 			(COND
-				((> N 3352)
+				((> N 3352)	;mayor a este numero se produce stackOverflow
 					"EROR: el numero suministrado supera el limite de ejecucion del programa"
 				)
 				((< N 0) ; el programa no considera la suma de numeros primos en "sentido negativo"
@@ -86,9 +87,10 @@
 ; Función cáscara auxiliar a la función isPrime
 (DEFUN isPrimeShell (N)
 	(LET ((B 2))
-		(isPrime N B)
+		(isPrime N B) ; llamada a la funcion que verficia si un numero es primo o no
 	)
 )
+
 ; Función auxiliar que calcula si un número dado "N" es primo, "B" es pasado por parámetro y empieza en 2. Se considera que 1 NO es primo.
 (DEFUN isPrime (N B)
 	(COND
@@ -111,19 +113,17 @@
 	)
 )
 ;-------------------------------------------EJERCICIO PERMUTACION LEXICA-------------------------------------------
-; Funcion que calcula la permutacion lexica de una lista L suministrada por parametro. Se asume que L se encuentra ordenada lexicograficamente
+; Funcion que calcula la permutacion lexica de una lista L suministrada por parametro. 
+; Se asume que L se encuentra ordenada lexicograficamente
 (DEFUN permLex (L)
 	(COND
-		((LISTP L)
+		((LISTP L) ; verifico que L sea efectivamente una lista
 			(COND
-				((NULL L)
-					NIL
-				)
-				((= 1 (LIST-LENGTH L))
+				((= 1 (LIST-LENGTH L)) ; la unica permutacion posible de una lista con un unico elemento es esa misma lista
 					(LIST L)
 				)
 				(T
-					(permute L L)
+					(permute L L) ; llamada a la funcion auxiliar que realiza la permutacion
 				)
 			)
 		)
@@ -133,36 +133,44 @@
 	)
 )
 
-;L1 es la lista de elementos que restan permutar
-;L2 es la lista real con todos los elementos
+; Funcion que realiza la permutacion de una lista, llevando registro en otra de que elementos faltan permutar
+; L1 es la lista de elementos que restan permutar
+; L2 es la lista real con todos los elementos
 (DEFUN permute (L1 L2)
 	(COND
-		((= (LIST-LENGTH L1) 0)
+		((= (LIST-LENGTH L1) 0) ; si no restan elementos por permutar el resultado de la permutacion es vacio
 			NIL
 		)
-		(T
-			(APPEND (addToALL (CAR L1) (permLex (DELETE (CAR L1) L2))) (permute (CDR L1) (reArrange (CAR L1) (DELETE (CAR L1) L2) (- (LIST-LENGTH L2) (- (LIST-LENGTH L1) 1)))))
+		(T ; caso contrario la permutacion lexica de una lista L es el resultado de concatenar la cabeza de L a las permutaciones lexicas del cuerpo
+			;y formar una lista entro esto y las permutaciones lexicas del resto de L
+			(APPEND 
+				(addToALL (CAR L1) (permLex (DELETE (CAR L1) L2))) 
+				(permute (CDR L1) (reArrange (CAR L1) (DELETE (CAR L1) L2) (- (LIST-LENGTH L2) (- (LIST-LENGTH L1) 1))))
+			) 	
 		)
 	)
 )
 
+; Funcion que, dada una lista de listas L, inserta el parametro E como cabeza de la misma
+; devuelve una lista con las listas modificadas
 (DEFUN addToALL (E L)
 	(COND
-		((= (LIST-LENGTH L) 0)
+		((= (LIST-LENGTH L) 0) ; si no hay mas elementos en la lista finaliza la recursion
 			NIL
 		)
-		(T
-			(CONS (CONS E (CAR L)) (addToALL E (CDR L)))
+		(T ; se realiza la concatenacion de E a la primer lista, luego se llama recursivamente
+			(CONS (CONS E (CAR L)) (addToALL E (CDR L))) 
 		)
 	)
 )
 
+; Funcion que, dado un elemento E y una lista L inserta el elemento en la posicion I de L
 (DEFUN reArrange (E L I)
 	(COND
-		((= I 0)
+		((= I 0) ; si la I vale 0 encontre la posicion donde debo insertar E
 			(CONS E L)
 		)
-		(T
+		(T ; caso contrario, tomo por separado el cuerpo de L, reduzco I en 1, llamo recursivamente y vuelvo a concatenar
 			(CONS (CAR L) (reArrange E (CDR L) (- I 1)))
 		)
 	)
